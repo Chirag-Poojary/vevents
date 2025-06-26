6f55ecd846f9f70ace28cbf5066d367fc0b46cb9
Chirag-Poojary <poojarychirag06@gmail.com>
2025-06-26 23:24:04 +0530

    pages added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Navbar from "@/src/components/Navbar";
 import StatusCard from "@/src/components/StatusCard";
-import Image from "next/image";
 import Login from "./login/page";
 import CreateProposal from "./dashboard/committee/CreateProposal";
 
